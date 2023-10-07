7c8bb2367f7f15afbf3fbb605e8121e1214d836b
Demansell <demansell2016@gmail.com>
2023-10-07 10:07:45 -0500

    Added Crud Endpoints for payments

--- HipHopPizzaWangs/Program.cs
+++ HipHopPizzaWangs/Program.cs
@@ -33,4 +33,77 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
+// payment endpoints 
+
+//get all Payments 
+
+app.MapGet("/HipHopPizzaWangs/Payments", (HipHopPizzaWangsDbContext db) =>
+{
+    List<Payment> payments = db.Payments.ToList();
+    if (payments.Count == 0)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(payments);
+});
+
+// get paymetns by Id
+
+app.MapGet("/HipHopPizzaWangs/Payments/{id}", (HipHopPizzaWangsDbContext db, int id) =>
+{
+    Payment payments = db.Payments.SingleOrDefault(c => c.Id == id);
+    if (payments == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(payments);
+});
+
+// Post a Payment
+
+app.MapPost("/HipHopPizzaWangs/Payments", (HipHopPizzaWangsDbContext db, Payment payment) =>
+{
+    try
+    {
+        db.Add(payment);
+        db.SaveChanges();
+        return Results.Created($"/HipHopPizzaWangs/Payments/{payment.Id}", payment);
+    }
+    catch (DbUpdateException)
+    {
+        return Results.NotFound();
+    }
+});
+
+
+// Update a paymenttype
+
+app.MapPut("api/HipHopPizzaWangs/Payments", async (HipHopPizzaWangsDbContext db, int id, Payment payment) =>
+{
+    Payment paymentToUpdate = await db.Payments.SingleOrDefaultAsync(payment => payment.Id == id);
+    if (paymentToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    paymentToUpdate.Id = payment.Id;
+    paymentToUpdate.PaymentType = payment.PaymentType;
+    
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+// delete payment type
+app.MapDelete("/HipHopPizzaWangs/Payments/{paymentId}", (HipHopPizzaWangsDbContext db, int paymentId) =>
+{
+    Payment deletePayment = db.Payments.FirstOrDefault(c => c.Id == paymentId);
+    if (deletePayment == null)
+    {
+        return Results.NotFound();
+    }
+    db.Remove(deletePayment);
+    db.SaveChanges();
+    return Results.Ok(deletePayment);
+});
+
 app.Run();
